f41cbd4db9c86894f319985e2992b73ba12c7274
grey920 <gyuwoonj@gmail.com>
2020-08-30 21:49:21 +0900

    Scanner로 입력받는 것으로 수정

--- doit_prac/src/basic100/_13/Main.java
+++ doit_prac/src/basic100/_13/Main.java
@@ -1,4 +1,7 @@
 package basic100._13;
+
+import java.util.Scanner;
+
 /*
 - 입력된 수(N)만큼 N행 N열의 형태로 아래와 같이 출력되는 숫자 사각형을 출력하시오
 - 4
@@ -13,8 +16,8 @@ package basic100._13;
 public class Main {
 
 	public static void main(String[] args) {
-
-		int n = 4;
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 		int[][] arr = new int[n][n];
 		
 		for(int i=0; i<n; i++) {
