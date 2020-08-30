865dc92468598ed385514d7d3bfe8bf45c092664
grey920 <gyuwoonj@gmail.com>
2020-08-30 21:45:04 +0900

    알고리즘 기초 100제 13번. 숫자사각형(4) - 겨운ver

---
+++ doit_prac/src/basic100/_13/Main.java
@@ -0,0 +1,35 @@
+package basic100._13;
+/*
+- 입력된 수(N)만큼 N행 N열의 형태로 아래와 같이 출력되는 숫자 사각형을 출력하시오
+- 4
+- 정답  :
+1 2 3 4
+2 4 6 8 
+3 6 9 12
+4 8 12 16
+
+중첩 반복문(for 혹은 while, 배열
+*/
+public class Main {
+
+	public static void main(String[] args) {
+
+		int n = 4;
+		int[][] arr = new int[n][n];
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j < n; j++) {
+				arr[i][j] = (i+1) * (j+1);
+			}
+		}
+		
+		// 출력
+		for(int i=0; i < n; i++) {
+			for (int j=0; j < n; j++) {
+				System.out.printf("%4d", arr[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+}
